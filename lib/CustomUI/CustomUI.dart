89c5c7ce43758e0e8ddb986b7a4a023ca74c94a7
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2022-07-22 01:24:45 +0530

    add internet permisison & integrate the emoji picker

--- lib/CustomUI/CustomUI.dart
+++ lib/CustomUI/CustomUI.dart
@@ -3,3 +3,11 @@ import 'package:flutter/cupertino.dart';
 Widget customBox({double height = 0.0, double width = 0.0}) {
   return SizedBox(height: height, width: width);
 }
+
+double phoneHeight(BuildContext context) {
+  return MediaQuery.of(context).size.height;
+}
+
+double phoneWidth(BuildContext context) {
+  return MediaQuery.of(context).size.width;
+}
--- lib/Screens/IndividualChatScreen.dart
+++ lib/Screens/IndividualChatScreen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/CustomUI/CustomUI.dart';
 import 'package:whatsapp_clone/Model/ChatModel.dart';
@@ -11,69 +14,162 @@ class IndividualChatScreen extends StatefulWidget {
 }
 
 class _IndividualChatScreenState extends State<IndividualChatScreen> {
+  bool show = false;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          appBar: AppBar(
-        actions: [
-          Icon(Icons.videocam),
-          customBox(width: 20),
-          Icon(Icons.call),
-          PopupMenuButton(itemBuilder: (context) {
-            return [
-              PopupMenuItem(child: Text("View contact")),
-              PopupMenuItem(child: Text("Media, links, and docs")),
-              PopupMenuItem(child: Text("Search")),
-              PopupMenuItem(child: Text("Disappearing messages")),
-              PopupMenuItem(child: Text("Wallpaper")),
-              PopupMenuItem(child: Text("More"))
-            ];
-          })
-        ],
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              widget.chatModel.name,
-              style: TextStyle(fontSize: 17),
-            ),
-            Text(
-              "last seen today at 18:05",
-              style: TextStyle(fontSize: 14),
-            )
+        backgroundColor: Color.fromARGB(255, 202, 218, 245),
+        appBar: AppBar(
+          actions: [
+            Icon(Icons.videocam),
+            customBox(width: 20),
+            Icon(Icons.call),
+            PopupMenuButton(itemBuilder: (context) {
+              return [
+                PopupMenuItem(child: Text("View contact")),
+                PopupMenuItem(child: Text("Media, links, and docs")),
+                PopupMenuItem(child: Text("Search")),
+                PopupMenuItem(child: Text("Disappearing messages")),
+                PopupMenuItem(child: Text("Wallpaper")),
+                PopupMenuItem(child: Text("More"))
+              ];
+            })
           ],
-        ),
-        leadingWidth: 80,
-        leading: Row(
-          children: [
-            customBox(width: 5),
-            InkWell(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              child: Row(
-                children: [
-                  const Icon(
-                    Icons.arrow_back,
-                  ),
-                  customBox(width: 5),
-                  CircleAvatar(
-                    backgroundColor: Colors.blueGrey,
-                    radius: 23,
-                    child: Icon(
-                      widget.chatModel.isGroup ? Icons.group : Icons.person,
-                      size: 25,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                widget.chatModel.name,
+                style: TextStyle(fontSize: 17),
               ),
-            ),
-          ],
+              Text(
+                "last seen today at 18:05",
+                style: TextStyle(fontSize: 14),
+              )
+            ],
+          ),
+          leadingWidth: 80,
+          leading: Row(
+            children: [
+              customBox(width: 5),
+              InkWell(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.arrow_back,
+                    ),
+                    customBox(width: 5),
+                    CircleAvatar(
+                      backgroundColor: Colors.blueGrey,
+                      radius: 23,
+                      child: Icon(
+                        widget.chatModel.isGroup ? Icons.group : Icons.person,
+                        size: 25,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      )),
+        body: Container(
+          height: phoneHeight(context),
+          width: phoneWidth(context),
+          child: Stack(
+            children: [
+              ListView(),
+              Padding(
+                padding: const EdgeInsets.only(left: 5, bottom: 8.0),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                              width: phoneWidth(context) - 55,
+                              child: Card(
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(55)),
+                                  child: TextFormField(
+                                      decoration: InputDecoration(
+                                          border: InputBorder.none,
+                                          prefixIcon: IconButton(
+                                            icon: Icon(Icons.emoji_emotions),
+                                            onPressed: () {
+                                              setState(() {
+                                                show = !show;
+                                              });
+                                            },
+                                          ),
+                                          suffixIcon: Row(
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: [
+                                              IconButton(
+                                                  onPressed: () {},
+                                                  icon:
+                                                      Icon(Icons.attach_file)),
+                                              IconButton(
+                                                  onPressed: () {},
+                                                  icon: Icon(Icons.camera_alt))
+                                            ],
+                                          ),
+                                          contentPadding: EdgeInsets.all(13),
+                                          hintText: "type a message...")))),
+                          customBox(height: 3),
+                          CircleAvatar(
+                              child: IconButton(
+                                  onPressed: () {}, icon: Icon(Icons.mic)))
+                        ],
+                      ),
+                      show ? emojiSelection() : Container()
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget emojiSelection() {
+    return Container(
+      height: 250,
+      width: phoneWidth(context),
+      child: EmojiPicker(
+          onEmojiSelected: (category, emoji) {
+            print(emoji);
+          },
+          onBackspacePressed: () {},
+          config: Config(
+              columns: 7,
+              emojiSizeMax: 32 *
+                  (Platform.isIOS
+                      ? 1.30
+                      : 1.0), // Issue: https://github.com/flutter/flutter/issues/28894
+              verticalSpacing: 0,
+              horizontalSpacing: 0,
+              initCategory: Category.RECENT,
+              bgColor: Color(0xFFF2F2F2),
+              indicatorColor: Colors.blue,
+              iconColor: Colors.grey,
+              iconColorSelected: Colors.blue,
+              progressIndicatorColor: Colors.blue,
+              showRecentsTab: true,
+              recentsLimit: 28,
+              tabIndicatorAnimDuration: kTabScrollDuration,
+              categoryIcons: const CategoryIcons(),
+              buttonMode: ButtonMode.MATERIAL)),
     );
   }
 }
