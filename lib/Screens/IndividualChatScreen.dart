aad40dc70834bddb246a6f9d8813388f078c7df0
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2022-07-23 00:47:42 +0530

    Complete options layout for media , money (contacts , payment , gallery, location,document)

--- lib/Screens/IndividualChatScreen.dart
+++ lib/Screens/IndividualChatScreen.dart
@@ -16,6 +16,7 @@ class IndividualChatScreen extends StatefulWidget {
 class _IndividualChatScreenState extends State<IndividualChatScreen> {
   FocusNode focusNode = FocusNode();
   bool show = false;
+  bool _showattach = false;
   TextEditingController _controller = TextEditingController();
   @override
   void initState() {
@@ -24,6 +25,7 @@ class _IndividualChatScreenState extends State<IndividualChatScreen> {
       if (focusNode.hasFocus) {
         setState(() {
           show = false;
+          _showattach = false;
         });
       }
     });
@@ -33,6 +35,7 @@ class _IndividualChatScreenState extends State<IndividualChatScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        resizeToAvoidBottomInset: true,
         backgroundColor: Color.fromARGB(255, 202, 218, 245),
         appBar: AppBar(
           actions: [
@@ -107,6 +110,9 @@ class _IndividualChatScreenState extends State<IndividualChatScreen> {
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
+                        Container(
+                          child: _showattach ? options() : null,
+                        ),
                         Row(
                           children: [
                             Container(
@@ -124,11 +130,15 @@ class _IndividualChatScreenState extends State<IndividualChatScreen> {
                                               icon: const Icon(
                                                   Icons.emoji_emotions),
                                               onPressed: () {
-                                                focusNode.unfocus();
-                                                focusNode.canRequestFocus =
-                                                    false;
                                                 setState(() {
-                                                  show = !show;
+                                                  focusNode.unfocus();
+                                                  focusNode.canRequestFocus =
+                                                      false;
+                                                  Future.delayed(
+                                                      Duration(seconds: 5), () {
+                                                    show = !show;
+                                                    _showattach = false;
+                                                  });
                                                 });
                                               },
                                             ),
@@ -136,7 +146,17 @@ class _IndividualChatScreenState extends State<IndividualChatScreen> {
                                               mainAxisSize: MainAxisSize.min,
                                               children: [
                                                 IconButton(
-                                                    onPressed: () {},
+                                                    onPressed: () {
+                                                      focusNode.unfocus();
+                                                      focusNode
+                                                              .canRequestFocus =
+                                                          false;
+                                                      setState(() {
+                                                        _showattach =
+                                                            !_showattach;
+                                                        show = false;
+                                                      });
+                                                    },
                                                     icon: const Icon(
                                                         Icons.attach_file)),
                                                 IconButton(
@@ -147,7 +167,7 @@ class _IndividualChatScreenState extends State<IndividualChatScreen> {
                                             ),
                                             contentPadding:
                                                 const EdgeInsets.all(13),
-                                            hintText: "type a message...")))),
+                                            hintText: "Message")))),
                             customBox(height: 3),
                             CircleAvatar(
                                 child: IconButton(
@@ -166,6 +186,10 @@ class _IndividualChatScreenState extends State<IndividualChatScreen> {
               if (show) {
                 setState(() {
                   show = false;
+                });
+              } else if (_showattach) {
+                setState(() {
+                  _showattach = false;
                 });
               } else {
                 Navigator.pop(context);
@@ -210,5 +234,71 @@ class _IndividualChatScreenState extends State<IndividualChatScreen> {
               categoryIcons: const CategoryIcons(),
               buttonMode: ButtonMode.MATERIAL)),
     );
+  }
+
+  Widget optionCretion(IconData icons, Color color, String title) {
+    return Column(
+      children: [
+        CircleAvatar(
+          backgroundColor: color,
+          radius: 25,
+          child: Icon(
+            icons,
+            color: Colors.white,
+            size: 23,
+          ),
+        ),
+        customBox(height: 8),
+        Text(title)
+      ],
+    );
+  }
+
+  Widget options() {
+    return Container(
+        padding: EdgeInsets.only(top: 10, bottom: 10),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(10)),
+        width: phoneWidth(context),
+        margin: EdgeInsets.only(bottom: 5, left: 7, right: 15),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                optionCretion(Icons.insert_drive_file, Colors.blue, "Document"),
+                customBox(height: 5),
+                optionCretion(Icons.headphones,
+                    Color.fromARGB(255, 255, 102, 0), "Audio"),
+                customBox(height: 5),
+                optionCretion(
+                    Icons.person, Color.fromARGB(255, 42, 161, 106), "Person"),
+              ],
+            ),
+            customBox(width: 35),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                optionCretion(Icons.photo_camera, Colors.pink, "Camera"),
+                customBox(height: 5),
+                optionCretion(Icons.location_pin,
+                    Color.fromARGB(255, 22, 190, 44), "Location"),
+              ],
+            ),
+            customBox(width: 35),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                optionCretion(Icons.photo_library,
+                    Color.fromARGB(255, 199, 30, 233), "Gallery"),
+                customBox(height: 5),
+                optionCretion(Icons.currency_rupee,
+                    Color.fromARGB(255, 42, 161, 106), "Payment"),
+              ],
+            )
+          ],
+        ));
   }
 }
