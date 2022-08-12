f3ae2fdc434e8a65eaf84bf2d6a68d39013e79b4
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2022-08-12 12:18:28 +0530

    complete profile seeting page design

--- lib/Screens/SettingsPage.dart
+++ lib/Screens/SettingsPage.dart
@@ -14,7 +14,7 @@ class _SettingsPageState extends State<SettingsPage> {
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: true,
-        title: Text(
+        title: const Text(
           "Settings",
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
@@ -29,16 +29,125 @@ class _SettingsPageState extends State<SettingsPage> {
               radius: 30,
               backgroundImage: AssetImage("assets/1.jpeg"),
             ),
-            title: Text("Dheeraj Prajapat"),
-            subtitle: Text(
+            title: const Text("Dheeraj Prajapat"),
+            subtitle: const Text(
               "Taste the success once... tounge want more",
               style: TextStyle(color: Colors.grey, fontSize: 11.5),
             ),
-            trailing: IconButton(onPressed: () {}, icon: Icon(Icons.qr_code)),
+            trailing:
+                IconButton(onPressed: () {}, icon: const Icon(Icons.qr_code)),
           ),
-          customBox(height: 20),
+          customBox(height: 10),
+
+          //account
           ListTile(
-            leading: Icon(Icons.key),
+              leading: Padding(
+                padding: EdgeInsets.only(top: 6),
+                child: Image.asset(
+                  "assets/key.png",
+                  color: Colors.grey,
+                  height: 30,
+                ),
+              ),
+              title: Text(
+                "Account",
+                style: TextStyle(color: Colors.black, fontSize: 14),
+              ),
+              subtitle: Text(
+                "Privacy, security, change number",
+                style: TextStyle(color: Colors.grey, fontSize: 12),
+              )),
+          //Chats
+          ListTile(
+              onTap: () {},
+              leading: const Padding(
+                  padding: EdgeInsets.only(top: 6),
+                  child: Icon(
+                    Icons.message,
+                    color: Colors.grey,
+                  )),
+              title: const Text(
+                "Chats",
+                style: TextStyle(color: Colors.black, fontSize: 14),
+              ),
+              subtitle: const Text(
+                "Theme, wallpapers, chat history",
+                style: TextStyle(color: Colors.grey, fontSize: 12),
+              )),
+          //Notifications
+          ListTile(
+              onTap: () {},
+              leading: const Padding(
+                  padding: EdgeInsets.only(top: 6),
+                  child: Icon(
+                    Icons.notifications_none,
+                    color: Colors.grey,
+                  )),
+              title: const Text(
+                "Notifications",
+                style: TextStyle(color: Colors.black, fontSize: 14),
+              ),
+              subtitle: const Text(
+                "Message, group & call tones",
+                style: TextStyle(color: Colors.grey, fontSize: 12),
+              )),
+          //Storage and data
+          ListTile(
+              onTap: () {},
+              leading: const Padding(
+                  padding: EdgeInsets.only(top: 6), child: Icon(Icons.storage)),
+              title: const Text(
+                "Storage and data",
+                style: TextStyle(color: Colors.black, fontSize: 14),
+              ),
+              subtitle: const Text(
+                "Network usage, auto download",
+                style: TextStyle(color: Colors.grey, fontSize: 12),
+              )),
+          //app language
+          ListTile(
+              leading: Padding(
+                padding: const EdgeInsets.only(top: 6),
+                child: Image.asset(
+                  "assets/earth.png",
+                  color: Colors.grey,
+                  height: 30,
+                ),
+              ),
+              title: const Text(
+                "App language",
+                style: TextStyle(color: Colors.black, fontSize: 14),
+              ),
+              subtitle: const Text(
+                "English(phone's language)",
+                style: TextStyle(color: Colors.grey, fontSize: 12),
+              )),
+          //Help
+          ListTile(
+              leading: Padding(
+                padding: EdgeInsets.only(top: 6),
+                child: Image.asset(
+                  "assets/key.png",
+                  color: Colors.grey,
+                  height: 30,
+                ),
+              ),
+              title: const Text(
+                "Help",
+                style: TextStyle(color: Colors.black, fontSize: 14),
+              ),
+              subtitle: const Text(
+                "Help center, contact us, privacy policy",
+                style: TextStyle(color: Colors.grey, fontSize: 12),
+              )),
+          //Invite
+          ListTile(
+            onTap: () {},
+            leading: const Padding(
+              padding: const EdgeInsets.only(left: 7),
+              child: Icon(Icons.group),
+            ),
+            title: const Text("Invite a friend"),
           )
         ]),
       ),
